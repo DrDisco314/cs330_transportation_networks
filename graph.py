91fdc8d3580af0e548078335a4b29e454ec6703e
dustykeyboard1 <mikesyanks02@icloud.com>
2024-03-06 14:00:11 -0500

    File Headers

--- graph.py
+++ graph.py
@@ -37,6 +37,8 @@ class Graph:
             self.graph[node1] = {}
         if node2 not in self.graph:
             self.graph[node2] = {}
+
+        # Symetric Implementation
         self.graph[node1][node2] = weight
         self.graph[node2][node1] = weight
 
