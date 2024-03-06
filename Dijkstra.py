a5c83d03f952a5c5a2e99b29410e5cd4f6a0d930
dustykeyboard1 <mikesyanks02@icloud.com>
2024-03-06 13:30:24 -0500

    Updated Citations

--- Dijkstra.py
+++ Dijkstra.py
@@ -17,7 +17,7 @@ class Dijkstra:
         while priority_queue:
             """
             Citation: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
-            Inspiration for using a min-heap with with Dijkstra algorithm
+            Inspiration for using a min-heap with with Dijkstra algorithm.
 
             Citation: https://docs.python.org/3/library/heapq.html
             Heap Queue alogirthm sets up a binary tree with parents less than or equal to children
