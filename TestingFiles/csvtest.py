956556c1b8c5573cf82d68180ec4f88c3e0be303
Michael Scoleri <mikesyanks02@icloud.com>
2024-04-03 22:04:44 -0400

    Functioning CH implementation

--- TestingFiles/csvtest.py
+++ TestingFiles/csvtest.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 sys.path.append("../cs330_transportation_networks")
 
@@ -8,7 +9,7 @@ from Algorithms.Dijkstra import Dijkstra
 
 def main():
     start_node = 247191
-    end_node = 16
+    end_node = 184
     mygraph = Graph()
 
     mygraph.read_from_csv_file("Data/NewYork_Edgelist.csv")
@@ -17,8 +18,10 @@ def main():
     #     print(key, value)
     #     input("yo")
     dijkstra = Dijkstra(mygraph)
+    start = time.time()
     shortest_path = dijkstra.find_shortest_path(start_node, end_node)
-
+    end = time.time()
+    print(f"Time taken: {(end-start)*1000} (ms)")
     (
         print(f"Shortest path from {start_node} to {end_node}: {shortest_path}")
         if shortest_path is not None
---
+++ TestingFiles/pandanatest.py
@@ -0,0 +1,53 @@
+import pandana as pdna
+import pandas as pd
+from pandas import DataFrame
+import csv
+from pandana import Network
+import time
+
+
+def load_csv(filename: str) -> DataFrame:
+    df = pd.read_csv(filename)
+    return df
+
+
+def create_pandanas_network(df: DataFrame) -> Network:
+    """Create 2 dataframes following format
+    https://udst.github.io/pandana/index.html
+    """
+
+    nodes = (
+        df[["START_NODE", "XCoord", "YCoord"]]
+        .drop_duplicates("START_NODE")
+        .set_index("START_NODE")
+    )
+    nodes.index.name = "node_id"
+    nodes.columns = ["x", "y"]
+
+    edges = df[["START_NODE", "END_NODE", "LENGTH"]]
+    edges.columns = ["from", "to", "weight"]
+
+    graph_network = net = pdna.Network(
+        nodes["x"], nodes["y"], edges["from"], edges["to"], edges[["weight"]]
+    )
+    return graph_network
+
+
+def find_shortest_path(net: Network):
+    start_node = 247191
+    end_node = 184
+    start = time.time()
+    shortest_distance = net.shortest_path(start_node, end_node)
+    end = time.time()
+    print(f"Time taken: {(end-start)*1000} (ms)")
+    print(shortest_distance)
+
+
+def main():
+    graphdf = load_csv("Data/NewYork_Edgelist.csv")
+    graph_network = create_pandanas_network(graphdf)
+    find_shortest_path(graph_network)
+
+
+if __name__ == "__main__":
+    main()
